a5a3324fc276db9ef9c5b52b4f7be3bdf271fc71
maksim.semenov <maksim.semenov@jetbrains.com>
2025-07-30 16:02:46 +0200

    lib commit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use default_rust_project::add;
+#![allow(dead_code)]
 
 fn main() {
     let a = 1;
     let b = 2;
     let result = add(a, b);
     println!("Hello, result is {result}");
+}
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
